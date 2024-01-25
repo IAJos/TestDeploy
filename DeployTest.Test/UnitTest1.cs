f73ad21f10833c2fbba3fad579c4a07abf48184d
CHIMI Jospin <chimikotieu@gmail.com>
2024-01-24 22:03:14 -0400

    first commit

---
+++ DeployTest.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DeployTest.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
